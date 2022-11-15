f028797723db2cb24c245af08a65c911819711bd
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-11-15 08:02:20 +0000

    s

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import markdoc from '@markdoc/next.js'
 const withMarkdoc = require('@markdoc/next.js');
 module.exports = withMarkdoc(/* options */)({
   pageExtensions: ['md', 'mdoc', 'js', 'jsx', 'ts', 'tsx']
