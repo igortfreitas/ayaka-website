6a78b2f5755bda63a3813f79c00aeef032dc3376
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-11-15 21:40:20 +0800

    Update acm-prog.js

--- pages/cm/acm-prog.js
+++ pages/cm/acm-prog.js
@@ -15,20 +15,8 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-      <div style={{
-            zIndex: -1,
-            position: 'fixed',
-            width: '100vw',
-            height: '100vh',
-            filter: 'brightness(0.3)'
-        }}>
-            <Image 
-                src={Background} 
-                alt="Ayaka" 
-                layout="fill" 
-                objectFit="cover" 
-                objectPosition='center'
-            ></Image></div>
+ 
+
         <h1 className={styles.title}>
           All commands
         </h1>
