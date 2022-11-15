6a815848778c70a37821a645916382d56b94f889
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-11-15 20:59:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import gbStyles from '../styles/globals.css'
 import Link from 'next/link'
 import Image from 'next/image'
 import Background from '../public/Ayaka.jpg'
