d12e5be804cdecf56393def55eeff52ddaaa00c9
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-11-15 21:20:25 +0800

    Update t.js

--- pages/in/t.js
+++ pages/in/t.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '/styles/Home.module.css'
 import Link from 'next/link'
-import Background from '../public/Ayaka.jpg'
+import Background from '.../public/Ayaka.jpg'
 export default function Home() {
   return (
     <div className={styles.container}>
