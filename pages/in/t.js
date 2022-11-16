e7512aa3eadb1ffbf980a9fe90d753415bcfc123
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-11-16 04:33:20 +0000

    Remove repeated background

--- pages/in/t.js
+++ pages/in/t.js
@@ -13,20 +13,6 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-       <div style={{
-            zIndex: -1,
-            position: 'fixed',
-            width: '100vw',
-            height: '100vh',
-            filter: 'brightness(0.3)'
-        }}>
-            <Image 
-                src={Background} 
-                alt="Ayaka" 
-                layout="fill" 
-                objectFit="cover" 
-                objectPosition='center'
-            ></Image></div>
         <h1 className={styles.title}>
           Thank you for inviting the bot!
         </h1>
